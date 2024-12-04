ef4f8b2dab478dc8b1db0e220566e6733048551b
29 <791603901@qq.com>
2024-12-04 16:57:03 +0800

    ✨ Implement `force-ansi`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    env::args_os,
+    process::{exit, Command},
+};
+
 fn main() {
-    println!("Hello, world!");
+    #[cfg(windows)]
+    nu_ansi_term::enable_ansi_support().ok();
+
+    let mut args = args_os().skip(1);
+    exit(match args.next() {
+        None => -1,
+        Some(program) => match Command::new(program).args(args).spawn() {
+            Err(err) => {
+                eprintln!("{}", err);
+                -2
+            }
+            Ok(mut child) => match child.wait() {
+                Err(err) => {
+                    eprintln!("{}", err);
+                    -3
+                }
+                Ok(code) => match code.code() {
+                    None => -4,
+                    Some(code) => code,
+                },
+            },
+        },
+    });
 }
