99e6eb0f16ec55bcee56b8dac04fc4412d04add3
29 <791603901@qq.com>
2024-12-04 16:29:44 +0800

    🎉 Update project settings

--- build.rs
+++
@@ -1,9 +0,0 @@
-use version_check::is_feature_flaggable;
-
-fn main() {
-    println!("cargo::rustc-check-cfg=cfg(nightly)");
-
-    if is_feature_flaggable() == Some(true) {
-        println!("cargo:rustc-cfg=nightly");
-    }
-}
